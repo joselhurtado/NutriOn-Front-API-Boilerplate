37b56eef9d7597dd204671d21a9a4446513e0d55
melissanohemi <melissa.castro1118@gmail.com>
2022-09-30 18:46:31 +0000

    added bmi, deleted diet circle

--- src/front/js/pages/Profile.js
+++ src/front/js/pages/Profile.js
@@ -2,33 +2,33 @@ import React from "react";
 import "../../styles/dashboard.css";
 import "../../styles/home.css";
 import RecipeCollection from "/src/front/js/component/RecipeCollection.js";
-
-
+import BMISection from "/src/front/js/component/BMISection.js";
 
 export default function Profile() {
   return (
     <div className="container">
       <div className="container">
         <div className="row information-container">
-          <div className="col-sm card text-center profile-pic-container" >
-              <img
-                src="https://images.theconversation.com/files/478225/original/file-20220809-24-orxjcu.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
-                className="card-img-top text-center profile-pic"
-                alt="..."
-              />
-               <div className="card-body">
-                <h1 className="card-text">
-                  BEYONCE
-                </h1>
-                <h4>
-                  Nutrition Goals
-                </h4>
+          <div className="col-sm card text-center profile-pic-container">
+            <img
+              src="https://images.theconversation.com/files/478225/original/file-20220809-24-orxjcu.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+              className="card-img-top text-center profile-pic"
+              alt="..."
+            />
+            <div className="card-body">
+              <h1 className="card-text">BEYONCE</h1>
+              <h4></h4>
             </div>
           </div>
-          <div
-            className="col-sm">
+          <div className="col-sm">
             <h1 className="card-title text-center mt-1">My Information</h1>
-            <div><label for="exampleFormControlInput1" className="form-label fullName" >Full Name</label>
+            <div>
+              <label
+                for="exampleFormControlInput1"
+                className="form-label fullName"
+              >
+                Full Name
+              </label>
               <input
                 type="text"
                 className="form-control fullName"
@@ -64,157 +64,65 @@ export default function Profile() {
                 placeholder="name@example.com"
               />
             </div>
-          </div>
-        </div>
-
-      { /* Diet and Calculator Section */}
-        
-        <div
-          className="row bmiDiet">
-          <div className="col-sm">
-            <div className="card p personalDietCircle">
-              <div className="card-body innerCircle">
-                <h1 className="card-title text-center dietCircleTitle">Vegetarian Diet</h1>
-                <p className="card-text text-center">My Weight Gain Journey</p>
-                <div className="card-text-diet">
-                  <ul className="list-group list-group-flush text-center">
-                    <li className="list-group-item">Age</li>
-                    <li className="list-group-item">Weight</li>
-                    <li className="list-group-item">Height</li>
-                    <li className="list-group-item">Goal Weight</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm">
-            <div className="card calorie-calculator">
-              <h1 className="card-title text-center mt-4 mb-4">
-                Calorie Calculator
-              </h1>
-              <div className="row">
-                <h1 className="card-title text-center text-muted">
-                  My Height
-                </h1>
-                <div className="col">
-                  <label
-                    for="exampleFormControlInput3"
-                    className="form-label bmiAge"
-                  ></label>
-                  <input
-                    type="text"
-                    className="form-control feetInput"
-                    id="exampleFormControlInput"
-                    placeholder="FEET"
-                  />
-                </div>
-                <div className="col">
-                  <label
-                    for="exampleFormControlInput3"
-                    className="form-label"
-                  ></label>
-                  <input
-                    type="text"
-                    className="form-control inchesInput"
-                    id="exampleFormControlInput"
-                    placeholder="INCHES"
-                  />
-                </div>
-              </div>
-              <div className="col-sm">
-                <h1 className="card-title text-center text-muted">
-                  My Weight
-                </h1>
-
+            <div className="otherInfo">
+              <div className="mb-3">
                 <label
-                  for="exampleFormControlInput3"
-                  className="form-label bmiAge"
-                ></label>
+                  for="exampleFormControlInput4"
+                  className="form-label age2Input"
+                >
+                  Age
+                </label>
                 <input
                   type="text"
-                  className="form-control ageInput"
-                  id="exampleFormControlInput"
-                  placeholder="lbs."
+                  className="form-control age2Input"
+                  id="exampleFormControlInput4"
+                  placeholder="Age"
                 />
               </div>
-              <div className="container mb-4">
-                <div className="row">
-                <h1 className="card-title text-center text-muted">
-                  gender
-                </h1>
-                </div>
-                <div className="d-flex justify-content-center">
-                  <div className="form-check m-2">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="gridRadios"
-                      id="gridRadios1"
-                      value="option1"
-                      checked
-                    ></input>
-                    <label className="form-check-label gender" for="gridRadios1">
-                      Female
-                    </label>
-                  </div>
-                  <div className="form-check m-2">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="gridRadios"
-                      id="gridRadios2"
-                      value="option2"
-                    ></input>
-                    <label className="form-check-label gender" for="gridRadios2">
-                      Male
-                    </label>
-                  </div>
-                  <div className="form-check m-2">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="gridRadios"
-                      id="gridRadios3"
-                      value="option3"
-                    ></input>
-                    <label className="form-check-label gender" for="gridRadios3">
-                      Other
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <select
-                className="form-select activity-level"
-                aria-label="Default select example" >
-                <option selected>Activity Level</option>
-                <option value="1">Sedentary</option>
-                <option value="2">Light</option>
-                <option value="3">Moderate</option>
-                <option value="4">Active</option>
-              </select>
-
-              <div className="d-flex justify-content-center m-4">
-                <button
-                  className="btn btn-orange"
-                  type="button"
+              <div className="mb-3">
+                <label
+                  for="exampleFormControlInput5"
+                  className="form-label heightInput"
                 >
-                  Calculate
-                </button>
-                <button className="btn btn-orange-outline" type="button">
-                  Clear
-                </button>
+                  Height
+                </label>
+                <input
+                  type="text"
+                  className="form-control heightInput"
+                  id="exampleFormControlInput5"
+                  placeholder="Height"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  for="exampleFormControlInput6"
+                  className="form-label weightInput"
+                >
+                  Weight
+                </label>
+                <input
+                  type="text"
+                  className="form-control weightInput"
+                  id="exampleFormControlInput6"
+                  placeholder="Weight"
+                />
               </div>
             </div>
           </div>
         </div>
+
+        <div>
+          <BMISection />
+        </div>
+
+        {/* Diet and Calculator Section */}
       </div>
 
       <div>
-        <RecipeCollection/>
+        <RecipeCollection />
       </div>
 
-      { /* Slider Meals Section */}
+      {/* Slider Meals Section */}
 
       <div className="col- favoriteCard">
         <div className="card">
